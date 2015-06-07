011af96cdf7e35b888a7c89866726afd1e2b90a4
wanghongcheng <shexing.w@gmail.com>
2015-06-07 23:41:13 +0800

    Add p01

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -3,5 +3,9 @@ package org.p99.scala
 object P01 {
 
   // TODO
-
+  def last[A](list: List[A]): A = list match{
+    case x:: Nil => x;
+    case x::xs => last(xs)
+    case Nil => throw new java.util.NoSuchElementException
+  }
 }
